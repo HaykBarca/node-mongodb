b0e9f0f74af52b319a628491490a80cd9c643630
HaykBarca <hayk19939563@gmail.com>
2018-12-01 20:13:03 +0400

    Corrections in db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://<HaykBarca>:<haykbarca19939563>@ds231941.mlab.com:31941/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://HaykBarca:haykbarca19939563@ds231941.mlab.com:31941/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {
     mongoose
