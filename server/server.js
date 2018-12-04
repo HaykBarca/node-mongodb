b8439c2e3409abbf97185da982ceadef80f81ac1
HaykBarca <hayk19939563@gmail.com>
2018-12-04 22:40:21 +0400

    Added tokens for auth

--- server/server.js
+++ server/server.js
@@ -97,6 +97,23 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ["email", "password"]);
+    let user = new User({
+        email: body.email,
+        password: body.password
+    });
+
+ 
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+ });
+
 app.listen(PORT, () => {
     console.log(`App is litening on port ${PORT}`);
 });
