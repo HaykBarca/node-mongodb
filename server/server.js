bf48c926416df4e44abe4400eef2a5baada0771e
HaykBarca <hayk19939563@gmail.com>
2018-12-08 14:15:23 +0400

    Added DELETE users/me/token method

--- server/server.js
+++ server/server.js
@@ -132,6 +132,14 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, (e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`App is litening on port ${PORT}`);
 });
